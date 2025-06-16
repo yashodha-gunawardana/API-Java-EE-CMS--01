650fb8eee4d5453d54e7402e0b2ed249f870668a
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-17 00:27:32 +0530

    redirect user to login page after logout

--- src/main/java/org/example/controller/LogOutServlet.java
+++ src/main/java/org/example/controller/LogOutServlet.java
@@ -6,16 +6,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import java.io.IOException;
+
 @WebServlet("/logout")
 public class LogOutServlet extends HttpServlet {
 
     @Override
-    protected void doGet (HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         HttpSession session = req.getSession(false);
 
         if (session != null) {
             session.invalidate();
         }
+
+        resp.sendRedirect(req.getContextPath() + "/login.jsp");
     }
 }
