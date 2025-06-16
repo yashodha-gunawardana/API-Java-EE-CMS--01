1a17993639f85d1c92ab7bd690d2c868859544ed
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-16 22:36:29 +0530

    create and configure a new basicDataSource object

--- src/main/java/org/example/DBCP/DataSource.java
+++ src/main/java/org/example/DBCP/DataSource.java
@@ -1,9 +1,16 @@
 package org.example.DBCP;
 
+import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 @WebListener
 public class DataSource implements ServletContextListener {
 
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        BasicDataSource ds = new BasicDataSource();
+    }
 }
