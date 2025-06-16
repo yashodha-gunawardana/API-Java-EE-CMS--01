c5d689cbdc75e828444e1624285feefc74ebea7b
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-17 01:18:14 +0530

    fetch all complaints and forward to admin_dashboard.jsp

--- src/main/java/org/example/controller/ListAllComplaint.java
+++ src/main/java/org/example/controller/ListAllComplaint.java
@@ -1,4 +1,26 @@
 package org.example.controller;
 
-public class ListAllComplaint {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.model.Complaint;
+
+import java.util.List;
+
+@WebServlet("/admin/complaints")
+public class ListAllComplaint extends HttpServlet {
+
+    @Override
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        try {
+            List<Complaint> complaints = ComplaintDAO.getAllComplaints(getServletContext());
+            req.setAttribute("complaints", complaints);
+            req.getRequestDispatcher("/admin_dashboard.jsp").forward(req, resp);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
 }
