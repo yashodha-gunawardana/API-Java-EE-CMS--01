35184ac11453331075436359de4a5df973b15c2d
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-16 22:51:51 +0530

    handle POST requests for login functionality

--- src/main/java/org/example/controller/LoginServlet.java
+++ src/main/java/org/example/controller/LoginServlet.java
@@ -2,7 +2,14 @@ package org.example.controller;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) {
+
+    }
 }
