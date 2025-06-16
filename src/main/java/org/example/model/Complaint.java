cb9e3f24d81910e1f05fc0b08c1e0db029d51875
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-16 21:45:22 +0530

    add complaint JavaBeans with lombok annotation and required fields

--- src/main/java/org/example/model/Complaint.java
+++ src/main/java/org/example/model/Complaint.java
@@ -1,4 +1,21 @@
 package org.example.model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
 public class Complaint {
+    private int complaintId;
+    private int employeeId;
+    private String title;
+    private String description;
+    private String status;
+    private String remarks;
+    private String date;
+
 }
