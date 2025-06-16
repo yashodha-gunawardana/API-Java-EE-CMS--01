a830ef0aed636335c6f45ff97c124ac843d89c61
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-16 21:48:34 +0530

    add user JavaBeans with lombok annotations and user-related fields

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,4 +1,18 @@
 package org.example.model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
 }
