0c7d9686c75d2ed90c1370761b38ad806480da3e
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-17 23:16:08 +0530

    only show employees to edit complaints

--- src/main/java/org/example/controller/EditComplaintServlet.java
+++ src/main/java/org/example/controller/EditComplaintServlet.java
@@ -7,13 +7,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.example.model.User;
 
+import java.io.IOException;
+
 @WebServlet("/editComplaint")
 public class EditComplaintServlet extends HttpServlet {
 
     @Override
-    protected void doPost (HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         HttpSession session = req.getSession();
         User user = (User) session.getAttribute("user");
+
+        if (user == null || !"Employee".equalsIgnoreCase(user.getRole())) {
+            resp.sendRedirect("error_message.jsp");
+            return;
+        }
     }
 }
